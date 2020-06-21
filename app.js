c658ef65ba0dcf9bf91c870b92cbcbcbeba50df0
wbusaka <wilbusaka@gmail.com>
2020-06-21 12:51:35 +0300

    changed port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let express = require('express')
 let app = express()
 let todoController = require('./controllers/todoController')
 //Heroku port declaration
-let port = process.env.PORT || 3000
+let PORT = process.env.PORT || 3000
 //template
 app.set('view engine', 'ejs')
 
@@ -13,5 +13,5 @@ app.use(express.static(__dirname + '/public'))
 todoController(app)
 
 //listen to port
-app.listen(port)
+app.listen(PORT)
 console.log('listening to port 3000')   
